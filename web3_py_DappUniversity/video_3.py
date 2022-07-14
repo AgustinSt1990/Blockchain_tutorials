e2eae8ea81fdebf6d78c52344161bb9b4e9c6d3b
AgustinSt1990 <af.stigliano@gmail.com>
2022-07-14 16:12:10 -0300

    Correction .env file

--- web3_py_DappUniversity/video_3.py
+++ web3_py_DappUniversity/video_3.py
@@ -1,14 +1,18 @@
 # Sending tx in eth
 
 from web3 import Web3
+import os
+from dotenv import load_dotenv
 
-ganache_url = 'http://127.0.0.1:7545'
+load_dotenv()
+ganache_url = os.environ['NODE_PROVIDER_LOCAL']
 web3 = Web3(Web3.HTTPProvider(ganache_url))
 
-account_1 = '0x6C4025A9970C32622Cb4Bc76b94d89Ffa072fb64'
-account_2 = '0x5B0C7808D97897c5E27FEaDCFac478Dcb2FE57cC'
 
-private_key = '2100213c322a5184dfca981a24722556dabfa34e60fc9f4b2828ebc640425e8d'
+account_1 = os.environ['ACCOUNT_1']
+account_2 = os.environ['ACCOUNT_2']
+
+private_key = os.environ['PRIVATE_KEY_1']
 
 # get nonce
 nonce = web3.eth.getTransactionCount(account_1)
